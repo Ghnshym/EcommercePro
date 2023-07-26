3af282269d05146d69614e56b320d90a0ddae202
ghanshyam <bca1902113@gmail.com>
2023-07-26 12:40:58 +0530

    commit 9

--- resources/views/home/product_details.blade.php
+++ resources/views/home/product_details.blade.php
@@ -78,7 +78,7 @@
       </div>
       <!-- jQery -->
       <script src="home/js/jquery-3.4.1.min.js"></script>
-      <!-- popper js -->
+     
       <script src="home/js/popper.min.js"></script>
       <!-- bootstrap js -->
       <script src="home/js/bootstrap.js"></script>

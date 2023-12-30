36148b755eb533be8c5b9d18d9e14d0e1a8ccfad
ghanshyam <bca1902113@gmail.com>
2023-12-30 17:26:27 +0530

    profile update configuration

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -14,7 +14,7 @@
       
    </head>
    <body>
-      <div class="hero_area">
+      @if(Auth::user()->usertype == 1)
          <header class="header_section">
             <div class="container">
                <nav class="navbar navbar-expand-lg custom_nav-container ">
@@ -58,6 +58,50 @@
                </nav>
             </div>
          </header>
+      @else
+         <header class="header_section">
+            <div class="container">
+               <nav class="navbar navbar-expand-lg custom_nav-container ">
+                  <a class="navbar-brand" href="{{ url('/redirect') }}"><img width="250" src="{{ asset('/images/logo.jpg') }}" alt="#" /></a>
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""> </span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                     <ul class="navbar-nav">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="{{ url('/redirect') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="{{url('/products')}}">Products</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="{{ url('/show_cart') }}">CarT</a>
+                       </li>
+                       <li class="nav-item">
+                          <a class="nav-link" href="{{ url('/user_order') }}">Order</a>
+                       </li>
+        
+                          @if(Route::has('login'))
+                          @auth
+                          <x-app-layout>
+        
+                          </x-app-layout>
+                          @else
+                             
+                        <li class="nav-item">
+                          <a class="btn btn-primary mx-4" href="{{ route('login') }}">Login</a>
+                       </li>
+                       <li class="nav-item">
+                          <a class="btn btn-success" href="{{ route('register') }}">Register</a>
+                       </li>
+                       @endauth
+                       @endif
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         </header>
+      @endif
          <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 @if (Laravel\Fortify\Features::canUpdateProfileInformation())
